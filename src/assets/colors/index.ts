a702170ada8348730d3ec0a021eabf3cf3a7cfc8
Leonardo Jaques <jaques.projetos@outlook.com>
2023-12-22 23:10:52 -0300

    feat: create component background screem and add assets

---
+++ src/assets/colors/index.ts
@@ -0,0 +1,6 @@
+export const colors = {
+  primary: '#142995',
+  secondary: '#12C2E9',
+  tertiary: '#A5FF32',
+  quaternary: '#111111',
+};
--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -3,15 +3,18 @@ import {NavigationContainer} from '@react-navigation/native';
 import {createStackNavigator} from '@react-navigation/stack';
 import HomeScreen from '@screens/home';
 
+enum RoutesEnums {
+  Home = 'Home',
+}
 export default function Routes() {
   const Stack = createStackNavigator();
 
   return (
     <NavigationContainer>
       <Stack.Navigator
-        initialRouteName="Home"
+        initialRouteName={RoutesEnums.Home}
         screenOptions={{headerShown: false}}>
-        <Stack.Screen name="Home" component={HomeScreen} />
+        <Stack.Screen name={RoutesEnums.Home} component={HomeScreen} />
       </Stack.Navigator>
     </NavigationContainer>
   );
