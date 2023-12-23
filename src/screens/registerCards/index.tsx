b258695827533ee8a879a9e1e06184cb496a2fc1
Leonardo Jaques <jaques.projetos@outlook.com>
2023-12-22 22:15:38 -0300

    feat: create componentes and rotes

---
+++ src/screens/registerCards/index.tsx
@@ -0,0 +1,10 @@
+import {Text, View} from 'react-native';
+import React from 'react';
+
+export default function RegisterCard() {
+  return (
+    <View>
+      <Text>RegisterCard</Text>
+    </View>
+  );
+}
